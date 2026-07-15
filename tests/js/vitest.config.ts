a3b079b9f5078bf4df266212bcd42e5a55125f7a
bq <adam@haglund.dev>
2026-07-15 23:11:17 +0200

    add ci job for running generated tests

--- tests/js/vitest.config.ts
+++ tests/js/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config"
 
 export default defineConfig({
 	test: {
+		reporters: ["verbose"],
 		experimental: { viteModuleRunner: false },
 		env: { NODE_ENV: "test" },
 		mockReset: true,
